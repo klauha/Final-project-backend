08751a3099d1f60761463ca1c68f72eaaa9767b7
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-04-27 13:05:32 +0200

    feat: created models User and Role

---
+++ src/entities/user-model.ts
@@ -0,0 +1,36 @@
+
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne } from "typeorm";
+import { Role } from "./Role/role-model";
+
+
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ type: "varchar", length: 25 })
+    name!: string;
+
+    @Column({ type: "varchar", length: 50, nullable: true })
+    surname!: string;
+
+    @Column({ type: "varchar", length: 255, unique: true })
+    email!: string;
+
+    @Column({ type: "varchar", length: 255 })
+    password!: string;
+
+    @ManyToOne(() => Role, role => role.users)
+    role!: Role;
+
+    @Column({ type: "boolean", default: true })
+    is_active!: boolean;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    created_at!: Date;
+
+    @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updated_at!: Date;
+}
