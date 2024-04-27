c463c058b56a364ef90f1bb3b4120795558d028d
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-04-27 10:00:06 +0200

    feat:database connected

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,21 @@
 import dotenv from "dotenv";
 import { app } from "./app"
-
+import { AppDataSource } from "./database/db";
 
 const PORT = process.env.PORT || 4000
 
 const startServer = () => {
-
-    app.listen(PORT, () => {
-        console.log(`server ${PORT} is running`);
+    AppDataSource.initialize()
+    .then(() => {
+        console.log('Database connected');
+        app.listen(PORT, () => {
+            console.log(`server ${PORT} is running`);
+        })
     })
-
+    .catch(error => { console.log(error) })
 }
+
 startServer()
-
-// // funcion comrueba conexion base de datos
-// AppDataSource.initialize()
-//     .then(() => {
-//         console.log('Database connected');
-//         app.listen(PORT, () => {
-//             console.log(`server ${PORT} is running`);
-//         })
-
-//     })
-
-//     .catch(error => { console.log(error) })
-
-
-
-
 
 
 
