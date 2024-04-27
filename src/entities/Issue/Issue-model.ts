d5d9071bdba82d1997554e1ec87327f27e638375
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-04-27 13:32:13 +0200

    feat: Add Issues  model

---
+++ src/entities/Issue/Issue-model.ts
@@ -0,0 +1,41 @@
+
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, ManyToOne, JoinColumn } from "typeorm";
+import { IssueType } from "../IssueType/IssueType-model";
+import { User } from "../user-model";
+import { Department } from "../Departament/Departament-model";
+import { EstadoDeIncidencia } from "../../database/migrations/1714209683768-issues";
+
+@Entity()
+export class Issue {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ type: "varchar", length: 255, nullable: false })
+    title!: string;
+
+    @Column({ type: "text", nullable: true })
+    description!: string;
+
+    @Column({ type: "enum", enum: EstadoDeIncidencia, default: EstadoDeIncidencia.ABIERTA, nullable: false })
+    status!: EstadoDeIncidencia;
+
+    @ManyToOne(() => IssueType, { nullable: false })
+    @JoinColumn({ name: "issue_type_id" })
+    issue_type!: IssueType;
+
+    @ManyToOne(() => User, { nullable: true })
+    @JoinColumn({ name: "user_id" })
+    user_id!: User;
+
+    @ManyToOne(() => Department, { nullable: true })
+    @JoinColumn({ name: "department_id" })
+    department!: Department;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    created_at!: Date;
+
+    @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updated_at!: Date;
+}
+
