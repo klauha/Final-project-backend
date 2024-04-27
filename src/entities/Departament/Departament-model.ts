cf5919b755ba6566bdfda623d65ceb219ff53333
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-04-27 13:21:04 +0200

    feat: Add Departament model

---
+++ src/entities/Departament/Departament-model.ts
@@ -0,0 +1,17 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Department {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ type: "varchar", length: 255, unique: true, nullable: false })
+    name!: string;
+
+    @CreateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    created_at!: Date;
+
+    @UpdateDateColumn({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updated_at!: Date;
+}
