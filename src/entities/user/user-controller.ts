b605d4d35eae1b7b14209423b29defe949d27ee5
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-04-28 17:12:04 +0200

    Update:getUsers route to include select fields

--- src/entities/user/user-controller.ts
+++ src/entities/user/user-controller.ts
@@ -5,14 +5,23 @@ import { User } from "./User-model";
 
 export const getUsers = async (req: Request, res: Response) => {
     try {
-        const users = await User.find()
+        const users = await User.find({ select: ["id", "name", "surname","email","is_active"] })
+
+        if (users.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No users found",
+            })
+        }
+
+
 
         res.status(200).json({
             suceess: true,
             message: "users retrieved successfully",
             data: users
         })
-        
+
     } catch (error) {
         res.status(500).json(
             {
