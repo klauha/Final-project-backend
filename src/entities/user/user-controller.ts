ca1ba7a3127a446c78715223fd65df615f249a24
Claudia.Alvaro <claudiaalvaro17@gmail.com>
2024-04-29 10:33:03 +0200

    Feat: add update and delete user

--- src/entities/user/user-controller.ts
+++ src/entities/user/user-controller.ts
@@ -38,12 +38,11 @@ export const getUsers = async (req: Request, res: Response) => {
 // -------------------GET-PROFILE-------------------------------------------------------------
 
 export const getProfile = async (req: Request, res: Response) => {
-    console.log(123);
-    
+
     try {
         console.log(req.tokenData);
         const userId = req.tokenData.userId;
-console.log(userId);
+
 
         const profile = await User.findOne(
             {
@@ -56,10 +55,14 @@ console.log(userId);
                     surname: true,
                     email: true
                 },
+            })
 
-
-            }
-        )
+        if (!profile) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            })
+        }
         res.status(200).json({
             suceess: true,
             message: "Profile retrieved successfully",
@@ -75,5 +78,97 @@ console.log(userId);
             }
         )
     }
+}
+// -------------------UPDATE USER-------------------------------------------------------------
 
+export const updateUserById = async (req: Request, res: Response) => {
+    try {
+
+        const userId = req.tokenData.userId
+        const userName = req.body.name
+        const userSurname = req.body.surname
+
+        const userToUpdate = await User.findOne(
+            {
+                where: {
+                    id: userId,
+                }
+            }
+        )
+        if (!userToUpdate) {
+            return res.status(404).json({
+                success: false,
+                messagge: "User not found",
+            })
+        }
+        const userUpdated = await User.update(
+            {
+                id: userId
+            },
+            {
+                name: userName,
+                surname: userSurname
+            }
+        )
+
+        if (!userUpdated) {
+            return res.status(500).json({
+                success: false,
+                message: "User update failed",
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            messagge: "User updated",
+            data: userUpdated
+        })
+
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            messagge: "User can't be  updated",
+            error: error
+        })
+    }
+}
+
+// -------------------DELETE USER-------------------------------------------------------------
+
+export const deleteUser = async (req: Request, res: Response)=> {
+    try {
+        const userId = req.params.id
+        const userToRemove: any = await User.findOneBy(
+            {
+                    id: parseInt(userId),
+            }
+        )
+            
+        const userDeleted = await User.remove(userToRemove)
+
+        if (!userDeleted) {
+            return res.status(500).json({
+                success: false,
+                message: "User deletion failed",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            messagge: "user deleted",
+            data: userDeleted
+        })
+
+
+
+
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            messagge: "User can't be  deleted",
+            error: error
+    
+        })
+    }
 }
--- src/entities/user/user.routes.ts
+++ src/entities/user/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProfile, getUsers } from "./user-controller";
+import { deleteUser, getProfile, getUsers, updateUserById } from "./user-controller";
 import { auth } from "../../middlewares/auth";
 
 
@@ -7,5 +7,7 @@ const router = Router();
 
 router.get('/', auth, getUsers)
 router.get('/profile', auth, getProfile)
+router.put('/profile', auth, updateUserById)
+router.delete('/:id', auth, deleteUser)
 
 export default router;
